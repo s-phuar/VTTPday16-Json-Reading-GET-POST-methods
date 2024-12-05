4e31fa050985f5465719ec9d7c3a1cf9d0405ebf
s-phuar <coolbeansphuar123@gmail.com>
2024-12-05 12:02:03 +0800

    updated notes for html list iteration and image display + MVM mapping + environment API + multistageDocker

--- workshop/src/main/java/VTTPday16/workshop/service/HttpBinService.java
+++ workshop/src/main/java/VTTPday16/workshop/service/HttpBinService.java
@@ -28,7 +28,7 @@ public class HttpBinService {
     public static final String GET_URL = "https://api.giphy.com/v1/gifs/search";
 
     //how do we set API key here?
-        //set using environment
+        //set using railway environment
    @Value("${giphy.api-key}")
    private String apiKey;
 
@@ -38,38 +38,38 @@ public class HttpBinService {
     //       limit=3
 
     public List<String> search(SearchParams params){
-    //build the url  with query param 
-    String url = UriComponentsBuilder  
-            .fromUriString(GET_URL)
-            .queryParam("api_key", apiKey)
-            .queryParam("q", params.query()) //using the new SearchParams record return class
-            .queryParam("limit", params.limit())
-            .queryParam("rating", params.rating())
-            .toUriString();
+        //build the url  with query param 
+        String url = UriComponentsBuilder  
+                .fromUriString(GET_URL)
+                .queryParam("api_key", apiKey)
+                .queryParam("q", params.query()) //using the new SearchParams record return class
+                .queryParam("limit", params.limit())
+                .queryParam("rating", params.rating())
+                .toUriString();
 
-    //consturct the request, payload will hold nothing
-    RequestEntity<Void> req = RequestEntity
-            .get(url)
-            .accept(MediaType.APPLICATION_JSON)
-            .build();
+        //consturct the request, payload will hold nothing
+        RequestEntity<Void> req = RequestEntity
+                .get(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
 
-    try{
-        //create template for sending request
-        RestTemplate template = new RestTemplate();
-        //consider the below
-            //String payload = template.getForObject(url, String.class);
-        //call the URL
-        ResponseEntity<String> resp = template.exchange(req, String.class);
-        //get the payload
-        String payload = resp.getBody();
-        return getImages(payload);
+        try{
+            //create template for sending request
+            RestTemplate template = new RestTemplate();
+            //consider the below
+                //String payload = template.getForObject(url, String.class);
+            //call the URL
+            ResponseEntity<String> resp = template.exchange(req, String.class);
+            //get the payload
+            String payload = resp.getBody();
+            return getImages(payload);
 
 
 
-    }catch (Exception ex){
-        ex.printStackTrace();
-        return List.of(); //returns an immutable empty List, cannot add/remove/modify elements in List after creation
-    }
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return List.of(); //returns an immutable empty List, cannot add/remove/modify elements in List after creation
+        }
 }
 
     private List<String> getImages(String json){ //insert payload body (String) and read it as Json
